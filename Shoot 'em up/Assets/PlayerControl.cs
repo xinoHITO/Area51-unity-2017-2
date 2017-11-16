2c5bdf49559fa673caef30cb9733fb6dd994f7ca
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-11-16 18:53:51 -0500

    Push inicial del proyecto

---
+++ Shoot 'em up/Assets/PlayerControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public int velocidad;
+
+	// Use this for initialization
+	void Start () {
+		CambiarColor ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movimiento ();
+		if (Input.GetKey (KeyCode.Space)) {
+			CambiarColor ();
+		}
+	}
+
+	void CambiarColor(){
+		GetComponent<Renderer> ().material.color = Random.ColorHSV();
+	}
+
+	void Movimiento(){
+		if (Input.GetKey(KeyCode.W)) {
+			transform.Translate (0, velocidad*Time.deltaTime, 0);	
+		}
+
+		if (Input.GetKey(KeyCode.S)) {
+			transform.Translate (0, -velocidad*Time.deltaTime, 0);	
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate (-velocidad*Time.deltaTime,0, 0);	
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Translate (velocidad*Time.deltaTime,0, 0);	
+		}
+	}
+}
