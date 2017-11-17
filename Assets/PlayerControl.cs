e3015f81c7fd091306634b29e7256e83c7018ec8
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-11-16 22:01:18 -0500

    Clase 02 - 16.11.17
    -declaracion y modificacion de variables
    -tipos diferentes de variables (int , float, bool)
    -uso de if y else

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-	public int velocidad;
+	//int se usa para almacenar numeros enteros
+	public int velocidadCaminar;
+	//float se usa para almacenar numeros decimales
+	public float velocidadCorrer;
+	//bool es un tipo de variable que solo puede tener dos valores : verdadero y falso
+	public bool canControl;
 
+	public int contador;
 	// Use this for initialization
 	void Start () {
+//		canControl = true;
+//		canControl = false;
+
+		//aqui le estamos asignando un valor int a una variable float
+		//esto es valido porque internamente se pueden transformar enteros a decimales
+	//	velocidadCorrer = velocidadCaminar + 2;
+
+		//un if puede tener como condicion cualquier expresion que resulte en un valor de true / false
+		if (canControl) {
+			Debug.Log ("can control esta prendido");
+		}//opcionalmente se puede agregar un else cuyo contenido se ejecuta cuando la condicion del if resulta falsa 
+		else {
+			Debug.Log ("can control esta apagado");
+		}
+
 		CambiarColor ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Movimiento ();
+		if (canControl) {
+			//ejecutamos nuestra funcion movimiento
+			Movimiento ();				
+		}
+		//queremos que cada vez que hagas 10 clicks ...cambies de color
+		if (Input.GetMouseButtonDown (0)) {
+			//incrementamos el contador
+			contador = contador + 1;
+			//preguntamos si el contador es exactamente igual a 10
+			if (contador == 10) {//podemos usar comparaciones numéricas dentro de un if
+				//cambiamos de color
+				CambiarColor ();
+				//reseteamos el contador de vuelta a cero
+				contador = 0;
+			}
+		}
+
+
 		if (Input.GetKey (KeyCode.Space)) {
 			CambiarColor ();
+			velocidadCaminar = 10;
 		}
 	}
 
@@ -23,21 +62,59 @@ public class PlayerControl : MonoBehaviour {
 		GetComponent<Renderer> ().material.color = Random.ColorHSV();
 	}
 
+	//creamos nuestra propia funcion llamada Movimiento
 	void Movimiento(){
-		if (Input.GetKey(KeyCode.W)) {
-			transform.Translate (0, velocidad*Time.deltaTime, 0);	
+		//detectamos click izquiero del mouse
+//		if (Input.GetMouseButtonDown(0)) {
+//			velocidadCaminar = 4;
+//		}
+//
+//		//detectamos click derecho del mouse
+//		if (Input.GetMouseButtonDown(1)) {
+//			velocidadCaminar = 20;
+//		}
+
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			//detectamos cuando presiones la tecla W
+			if (Input.GetKey (KeyCode.W)) {
+				//movemos el personaje en el eje Y (osea hacia arriba)
+				transform.Translate (0, velocidadCorrer * Time.deltaTime, 0);	
+			}
+
+			if (Input.GetKey (KeyCode.S)) {
+				transform.Translate (0, -velocidadCorrer * Time.deltaTime, 0);	
+			}
+
+			if (Input.GetKey (KeyCode.A)) {
+				transform.Translate (-velocidadCorrer * Time.deltaTime, 0, 0);	
+			}
+
+			if (Input.GetKey (KeyCode.D)) {
+				transform.Translate (velocidadCorrer * Time.deltaTime, 0, 0);	
+			}
+		} else {
+			//detectamos cuando presiones la tecla W
+			if (Input.GetKey(KeyCode.W)) {
+				//movemos el personaje en el eje Y (osea hacia arriba)
+				transform.Translate (0, velocidadCaminar*Time.deltaTime, 0);	
+			}
+
+			if (Input.GetKey(KeyCode.S)) {
+				transform.Translate (0, -velocidadCaminar*Time.deltaTime, 0);	
+			}
+
+			if (Input.GetKey(KeyCode.A)) {
+				transform.Translate (-velocidadCaminar*Time.deltaTime,0, 0);	
+			}
+
+			if (Input.GetKey(KeyCode.D)) {
+				transform.Translate (velocidadCaminar*Time.deltaTime,0, 0);	
+			}
+		
 		}
 
-		if (Input.GetKey(KeyCode.S)) {
-			transform.Translate (0, -velocidad*Time.deltaTime, 0);	
-		}
 
-		if (Input.GetKey(KeyCode.A)) {
-			transform.Translate (-velocidad*Time.deltaTime,0, 0);	
-		}
 
-		if (Input.GetKey(KeyCode.D)) {
-			transform.Translate (velocidad*Time.deltaTime,0, 0);	
-		}
 	}
 }
