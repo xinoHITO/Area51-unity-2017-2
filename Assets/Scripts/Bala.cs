a25aa446df254bb7410990115e13025fc05397b4
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-11-23 21:54:16 -0500

    clase 03
    disparo normal que spawnea balas cada x segundos mientras mantengas el click presionado
    disparar bombas que se gastan en base a un contador
    uso de OnTriggerEnter para destruir enemigos

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -13,4 +13,17 @@ public class Bala : MonoBehaviour {
 	void Update () {
 		transform.Translate (velocidad * Time.deltaTime, 0, 0);
 	}
+
+	//la funcion OnTriggerEnter requiere que ya sea el trigger o el objeto que se mete dentro del trigger
+	//al menos uno debe tener el componente Rigidbody
+
+	//se ejecuta cuando algun objeto entra en la zona del trigger 
+	void OnTriggerEnter(Collider other){
+		//si el objeto que ha entrado al trigger tiene el nombre "Enemigo" ... lo destruimos
+		if (other.name == "Enemigo") {
+			Destroy (other.gameObject);
+		}
+		//nos auto destruimos
+		Destroy	(gameObject);
+	}
 }
--- Assets/Scripts/ShootControl.cs
+++ Assets/Scripts/ShootControl.cs
@@ -7,6 +7,13 @@ public class ShootControl : MonoBehaviour {
 	public GameObject balaPrefab2;
 	public GameObject spawnBalas;
 	public GameObject spawnBalas2;
+
+	public int cantidadBombas = 5;
+	//Cuando usas private delante de una variable... ésta ya no aparecerá en el editor.
+	//Como esta variable es enteramente controlada internamente por el script
+	//no tiene sentido que este visible en el editor
+	private float tiempoDisparo = 0;
+	public float intervaloDisparo = 0.2f;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,10 +24,30 @@ public class ShootControl : MonoBehaviour {
 		//la funcion Instantiate crea una copia del objeto que le pasamos.
 		//Luego Unity coloca esta copia en una coordenada y rotacion especifica 
 		//dentro de la escena.
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			//Input.GetMouseButtonDown
-			Instantiate (balaPrefab, spawnBalas.transform.position, Quaternion.identity);
-			Instantiate (balaPrefab2, spawnBalas2.transform.position, Quaternion.identity);
+		if (Input.GetMouseButton (0)) {
+			//tiempoDisparo cuenta la cantidad de tiempo que tenemos
+			//presionado el click izquierdo
+			tiempoDisparo = tiempoDisparo + Time.deltaTime;
+			//revisamos si nuestro contador ya superó el intervalo de tiempo
+			//para disparar
+			if (tiempoDisparo >= intervaloDisparo) {
+				Instantiate (balaPrefab, spawnBalas.transform.position, Quaternion.identity);
+				tiempoDisparo = 0;
+			}
+		}
+		//al soltar el click seteamos el contador igual al intervalo
+		//esto lo hacemos para que la proxima vez que hagas click
+		//el primer disparo sea instataneo (apenas presiones el mouse)
+		if (Input.GetMouseButtonUp (0)) {
+			tiempoDisparo = intervaloDisparo;	
+		}
+
+		if (Input.GetMouseButtonDown (1)) {
+			if (cantidadBombas > 0) {
+				Instantiate (balaPrefab2, spawnBalas2.transform.position, Quaternion.identity);
+				cantidadBombas = cantidadBombas - 1;
+			}
+
 		}
 
 	}
